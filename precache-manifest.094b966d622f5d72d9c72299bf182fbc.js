42faecf914a1184d6e08f40236c9e7631a6958d0
AFcodCloud <alfibrz@gmail.com>
2020-11-01 15:07:32 +0800

    Updates

---
+++ precache-manifest.094b966d622f5d72d9c72299bf182fbc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b98674ae3db57bc90abf7b34f725d3",
+    "url": "/AFcodCloud/sapper/index.html"
+  },
+  {
+    "revision": "4fa90d33c1f67243a79c",
+    "url": "/AFcodCloud/sapper/static/css/main.d6bf13ad.chunk.css"
+  },
+  {
+    "revision": "7496173844425fcb8c96",
+    "url": "/AFcodCloud/sapper/static/js/2.c89eec60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AFcodCloud/sapper/static/js/2.c89eec60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa90d33c1f67243a79c",
+    "url": "/AFcodCloud/sapper/static/js/main.aced5320.chunk.js"
+  },
+  {
+    "revision": "73b6f761e5db0270a825",
+    "url": "/AFcodCloud/sapper/static/js/runtime-main.5ac6f94d.js"
+  },
+  {
+    "revision": "0f2555b14520eb166eac7b89cc555cdb",
+    "url": "/AFcodCloud/sapper/static/media/0.0f2555b1.jpg"
+  },
+  {
+    "revision": "fab3e397108234c0796368361fd2065e",
+    "url": "/AFcodCloud/sapper/static/media/2.fab3e397.jpg"
+  },
+  {
+    "revision": "c49f3a1cb6a377205d19efba89452118",
+    "url": "/AFcodCloud/sapper/static/media/3.c49f3a1c.jpg"
+  },
+  {
+    "revision": "8e3abd98ef8e2ea978837c0dc1c6b1dd",
+    "url": "/AFcodCloud/sapper/static/media/5.8e3abd98.jpg"
+  },
+  {
+    "revision": "cb20c57635fd65bdc8c4b58a0fddf504",
+    "url": "/AFcodCloud/sapper/static/media/6.cb20c576.jpg"
+  },
+  {
+    "revision": "3f4316789a70335c1453929ef523614c",
+    "url": "/AFcodCloud/sapper/static/media/8.3f431678.jpg"
+  },
+  {
+    "revision": "4c5e53d1172f3d82ca6982cb732ed56a",
+    "url": "/AFcodCloud/sapper/static/media/9.4c5e53d1.jpg"
+  },
+  {
+    "revision": "d60b8dbb5f72d5d4ff310ab777879987",
+    "url": "/AFcodCloud/sapper/static/media/blow.d60b8dbb.png"
+  },
+  {
+    "revision": "b7a16fe41e5623922a08172bebba0491",
+    "url": "/AFcodCloud/sapper/static/media/bomb.b7a16fe4.png"
+  },
+  {
+    "revision": "483e693012e5cb95306d9e1683de03f4",
+    "url": "/AFcodCloud/sapper/static/media/flag.483e6930.png"
+  },
+  {
+    "revision": "c482d1ba8b8a405686db882cea88ed85",
+    "url": "/AFcodCloud/sapper/static/media/lose.c482d1ba.png"
+  },
+  {
+    "revision": "5355c43e2201230c5f966f134b2f4c71",
+    "url": "/AFcodCloud/sapper/static/media/play.5355c43e.png"
+  },
+  {
+    "revision": "1331eb28611c2040bb4abdd9908ab336",
+    "url": "/AFcodCloud/sapper/static/media/win.1331eb28.png"
+  }
+]);
